bb3b256782840291ef47fb5030e5020fad86726d
junaidjmomin <121440706+junaidjmomin@users.noreply.github.com>
2023-10-26 21:10:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,26 @@
-/* Basic Reset */
-* {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
+// Dummy data for Netflix recommendations
+const recommendations = [
+    { title: "Stranger Things", image: "url_to_image_1.jpg" },
+    { title: "The Crown", image: "url_to_image_2.jpg" },
+    // Add more recommendations here
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadRecommendations();
+
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        alert('Thank you for your message!');
+    });
+});
+
+function loadRecommendations() {
+    const container = document.getElementById('netflix-container');
+    recommendations.forEach(rec => {
+        const div = document.createElement('div');
+        div.className = 'netflix-item';
+        div.innerHTML = `<h3>${rec.title}</h3><img src="${rec.image}" alt="${rec.title}">`;
+        container.appendChild(div);
+    });
 }
 
-body {
-    font-family: 'Arial', sans-serif;
-    line-height: 1.6;
-    color: #333;
-    background: #f4f4f4;
-}
-
-.container {
-    max-width: 1100px;
-    margin: auto;
-    padding: 0 20px;
-}
-
-header {
-    background: #333;
-    color: #fff;
-    padding: 20px 0;
-}
-
-.header-inner {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-
-nav a {
-    color: #fff;
-    text-decoration: none;
-    margin: 0 10px;
-}
-
-nav a:hover {
-    color: #ddd;
-}
-
-.info-section {
-    padding: 40px 0;
-}
-
-.bg-alt {
-    background: #eaeaea;
-}
-
-.recommendations-grid {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-    gap: 20px;
-}
-
-.recommendation-item {
-    background: #fff;
-    padding: 20px;
-    box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-}
-
-footer {
-    background: #333;
-    color: #fff;
-    text-align: center;
-    padding: 20px 0;
-    margin-top: 40px;
-}
-
-/* Responsive Design */
-@media (max-width: 768px) {
-    .header-inner, .recommendations-grid {
-        flex-direction: column;
-    }
-}
