7a4a4a2139c12cb35820b19bc6a0019d46305f03
junaidjmomin <121440706+junaidjmomin@users.noreply.github.com>
2023-10-26 20:56:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thanks for reaching out! (Note: This form is a placeholder and does not send your message.)');
+});
